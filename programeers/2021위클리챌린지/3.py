729dc3b0093c329a9dbbae38bc9e29e0d0a65f1a
woongjoonchoi <oongjoon10@gmail.com>
2021-11-22 07:58:18 +0900

    <feat> modify 3.py
    dist==1 ->dist =1
    design pattern

--- programeers/2021위클리챌린지/3.py
+++ programeers/2021위클리챌린지/3.py
@@ -1,5 +1,6 @@
 from collections import deque
 from collections import defaultdict
+import copy
 dx = [1,-1, 0,0]
 dy = [0,0,1,-1]
 
@@ -10,7 +11,8 @@ def rotate(x,y,i):
         return [y,-x]
     elif i==2 :
         return [-y,x]
-
+def rotate_b(x,y,n) :
+    return [y,n-1-x]
 def all_b(ans) :
     new_ans = []
     new_ans.append(ans) 
@@ -24,6 +26,7 @@ def all_b(ans) :
 def go_zero(ans) :
     min_x = 100
     min_y = 100
+    ans=copy.copy(ans)
     for x,y in ans :
         min_x = min(min_x , x)
         min_y = min(min_y , y)
@@ -44,13 +47,14 @@ def bfs(matrix ,x , y,color) :
     while q :
         s_x,s_y = q.popleft()
         dist[s_x][s_y] = 1
-        ans.append([s_x, s_y])
-        matrix[s_x][s_y] = 1-color
         for i in range(4):
             n_x = s_x + dx[i]
             n_y = s_y + dy[i]
             if check(n_x, n_y , n ) and matrix[n_x][n_y] ==color and dist[n_x][n_y] == 0 :
+                dist[n_x][n_y] = 1
                 q.append((n_x,n_y))
+        dist[s_x][s_y] = 2
+        ans.append([s_x, s_y])
     return ans
 def idd(s,g_z) :
     g_z.sort()
@@ -73,28 +77,33 @@ def solution(game_board, table):
     answer = 0
     t = len(table)
     g = len(game_board)
-    all_t = defaultdict(list)
-    check_t=defaultdict(list)
+    all_t = {}
+    check_t={}
     for i in range(t) :
         for j in range(t) :
             if table[i][j] == 1 :
-                s_z = go_zero(bfs(table, i, j,1))
+                s_z = bfs(table, i, j,1)
+                for b1 ,b2 in s_z :
+                    table[b1][b2] = 0
+                s_z = go_zero(s_z)
+                if len(s_z) not in all_t.keys():
+                    all_t[len(s_z)] =[]
+                    check_t[len(s_z)] =[]
                 all_t[len(s_z)].append(all_b(s_z))
                 check_t[len(s_z)].append(0)
     for i in range(g) :
         for j in range(g) :
             if game_board[i][j] == 0 :
-                g_z = go_zero(bfs(game_board, i, j,0))
+                o_g = bfs(game_board, i, j,0)      
+                g_z = go_zero(copy.deepcopy(o_g))
                 len_gb = len(g_z)
                 if len_gb in all_t.keys():
-                    # print('yes')
                     for ind,s in enumerate(all_t[len_gb]) :
                         if check_t[len_gb][ind] == 1 : continue
                         if idd(s,g_z) == True :
                             check_t[len_gb][ind] = 1
                             answer+=len_gb
+                            for g1,g2 in o_g:
+                                game_board[g1][g2] = 1
                             break
-                                    
-                        
-    # print(check_t)
     return answer
