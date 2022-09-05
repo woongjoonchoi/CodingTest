151f14c8327655bb73c885e23a8f272c57674967
woongjoonchoi <oongjoon10@gmail.com>
2022-09-06 08:03:32 +0900

    feat progreammers
    카카오 채용연계 미로탈출

--- programeers/2021카카오채용연계/미로탈출.py
+++ programeers/2021카카오채용연계/미로탈출.py
@@ -1,9 +1,10 @@
 import heapq as hq
 
 def solution(n, start, end, roads, traps):
-    answer = 0
+    answer = float('inf')
     
-    adj = {}
+    adj = {}  # key : src, val :  dst, weight
+
     trap_bit = 0
     
 #     traps의 i-th 를 방문하면 오른쪽에서 i-th bit만큼을 on
@@ -17,40 +18,61 @@ def solution(n, start, end, roads, traps):
             adj[u] = []
             
         adj[u].append((v,w))
+        
+    for i in range(1,n+1) :
+        if i not in adj.keys() :
+            adj[i] = []
     
     queue = []
     
     hq.heappush(queue,(start,0))
     dist[trap_bit][start]  = 0
 
-    past_node = -1
+    # past_node = -1
     while queue :
         u, est = hq.heappop(queue)
         u= u%10000
         for i , t in enumerate(traps) : 
             if u == t:
                 trap_bit = trap_bit ^ (1 << i)
-                for v , w in adj[past_node] :
-                    adj[u].delete(v,w)
-                    if v not in adj.keys() :
-                        adj[v] = []
-                    adj[v].append(u,w)
-                
+
+                for j , (src, dst , w)  in enumerate(roads) : 
+                    # if src == u :
+                    #     adj[src].remove((dst,w))
+                    #     if dst not in adj.keys() :
+                    #         adj[dst] = []
+                    #     adj[dst].append((src,w))
+                    # if dst == u :
+                    if not(src == u or dst == u)  :
+                        continue
+                    adj[src].remove((dst,w))
+                    # if dst not in adj.keys() :
+                    #     adj[dst] = []
+                    adj[dst].append((src,w))
+                    
+                    roads[j] = (dst,src,w)
             
         
         for v , w in adj[u] :
-            if dist[trap_bit][v] > dist[trap_bit][u] + w :
-                dist[trap_bit][v] = dist[trap_bit][u] + w 
-                hq.heappush(queue,(10000 * trap_bit + v,dist[trap_bit][v]))
+            temp_bit = 0
+            for i , t in enumerate(traps) : 
+                if v == t :
+                    temp_bit = 1 << i
+            if dist[trap_bit^temp_bit][v] > dist[trap_bit][u] + w :
+                dist[trap_bit ^ temp_bit][v] = dist[trap_bit][u] + w 
+                hq.heappush(queue,(10000 * (trap_bit ^temp_bit) + v,dist[trap_bit^temp_bit][v]))
                 
-        past_node = u
+        # past_node = u
                 
                 
         
 
     
     for i in range(1024) :
-        answer = min(dist[i][end])
+        answer = min(dist[i][end],answer)
+    print(answer)
     return answer
 
-solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2])
+# solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2])
+
+solution(	4, 1, 4, [[1, 2, 1], [3, 2, 1], [2, 4, 1]], [2, 3])
