1061b1f460f9bfb7e770efa4df7d8d58cd7c576a
woongjoonchoi <oongjoon10@gmail.com>
2022-09-11 07:41:56 +0900

    feat progreammers
    미로탈출
    
    finish

--- programeers/2021카카오채용연계/미로탈출.py
+++ programeers/2021카카오채용연계/미로탈출.py
@@ -5,78 +5,81 @@ def solution(n, start, end, roads, traps):
     
     
     
-    adj = [{} for _ in range(2**len(traps))] # key : src, val :  dst, weight]
+    adj = [{} for _ in range(2**len(traps))] #  i , : which_trap visit , key : src, val :  dst, weight 
 
     trap_bit = 0
     
 #     traps의 i-th 를 방문하면 오른쪽에서 i-th bit만큼을 on
 #     재 방문시 off
     
-    dist = [[float('inf') for _  in range(n+1)] for _ in range(2**len(traps))]
+    # dist = [[float('inf') for _  in range(n+1)] for _ in range(2**len(traps))]
+    
+    dist = {}
 #     distance estimate from s 
     
-    for i in range(2**len(traps)) :
+    
+    def make_adj(i) :
+    # for i in range(2**len(traps)) :
         roads_copy = roads[:]
-        
-        bin_traps = bin(i)[2:]
-        
+        bin_str = bin(i)[2:]
+
+        bin_traps = (len(traps)-len(bin_str)) * "0"+bin_str
+
+        for k in range(1,n+1) :
+            adj[i][k] = []
         for j , bits  in enumerate(bin_traps) :
+
             if bits == "1" :
-                t = 
-                for src, dst, w in roads_copy :
-                    if not()
-    
-    for u , v, w  in roads :
-        if u not in adj.keys() :
-            adj[u] = []
+                for ind , (src, dst, w )in enumerate(roads_copy) :
+                    
+                    if not(src == traps[len(traps) -1-j]  or dst == traps[len(traps)-1-j]) :
+                        continue
+                    roads_copy[ind] = (dst,src,w)
+        for j ,(src, dst, w )in enumerate(roads_copy) :
+            adj[i][src].append((dst,w))
             
-        adj[u].append((v,w))
-        
-    for i in range(1,n+1) :
-        if i not in adj.keys() :
-            adj[i] = []
-    
+
     queue = []
     
-    for i in range(2 ** len(traps)) :
-        for j in range(1,n+1) :
-            queue.append((dist[i][j] , 10000 * i + j ))
+    # for i in range(2 ** len(traps)) :
+    #     for j in range(1,n+1) :
+    #         queue.append((dist[i][j] , 10000 * i + j ))
     # hq.heappush(queue,(start,0))
+    
+    
     hq.heappush(queue,(0,start))
     
     hq.heapify(queue)
+    
+    dist[0] = [float('inf') for _  in range(n+1)]
     dist[trap_bit][start]  = 0
 
     # past_node = -1
     while queue :
         # u, est = hq.heappop(queue)
         est , u = hq.heappop(queue)
+        trap_bit = u //10000
         u= u%10000
-        for i , t in enumerate(traps) : 
-            if u == t:
-                trap_bit = trap_bit ^ (1 << i)
 
-                for j , (src, dst , w)  in enumerate(roads) : 
-
-                    if not(src == u or dst == u)  :
-                        continue
-                    
-          
-                    adj[src].remove((dst,w))
-
-                    adj[dst].append((src,w))
-           
-                    roads[j] = (dst,src,w)
+        if not adj[trap_bit].keys() :
+            make_adj(trap_bit)
             
-        
-        for v , w in adj[u] :
+        if trap_bit not in dist.keys() :
+            dist[trap_bit] = [float('inf') for _  in range(n+1)]
+        for v , w in adj[trap_bit][u] :
             temp_bit = 0
             for i , t in enumerate(traps) : 
                 if v == t :
-                    temp_bit = 1 << i
+                    temp_bit = 1 << (i)
+            if trap_bit^temp_bit not in dist.keys() :
+                dist[trap_bit^temp_bit] = [float('inf') for _  in range(n+1)]
             if dist[trap_bit^temp_bit][v] > dist[trap_bit][u] + w :
                 dist[trap_bit ^ temp_bit][v] = dist[trap_bit][u] + w 
                 hq.heappush(queue,(dist[trap_bit^temp_bit][v] , 10000 * (trap_bit ^temp_bit) + v))
+                
+    
+        if u == end :
+            break
                 
         # past_node = u
                 
@@ -85,6 +88,8 @@ def solution(n, start, end, roads, traps):
 
     
     for i in range(2**len(traps)) :
+        if i not in dist.keys() :
+            continue
         answer = min(dist[i][end],answer)
     # print(answer)
     if answer == float('inf') :
