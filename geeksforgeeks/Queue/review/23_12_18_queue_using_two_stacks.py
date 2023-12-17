6b790b45c3c5b5bbbad34a88f5351c89ac3f4682
woongjoonchoi <oongjoon10@gmail.com>
2023-12-18 06:16:04 +0900

    feat : Review
     queue using two stacks

---
+++ geeksforgeeks/Queue/review/23_12_18_queue_using_two_stacks.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+class Queue:
+    def __init__(self):
+        self.s1 = []  ## enque
+        self.s2 = [] ## deque
+    
+    def enqueue(self,X):
+        self.s1.append(X)
+        # code here
+    def dequeue(self):
+        # code here
+        ans = None
+
+        while len(self.s1) >1 :
+            self.s2.append(self.s1.pop())
+        ans = self.s1.pop()
+        
+        while len(self.s2) >0 :
+            self.s1.append(self.s2.pop())
+            
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        ob=Queue()
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        i = 0
+        while i<len(a):
+            if a[i] == 1:
+                ob.enqueue(a[i+1])
+                i+=1
+            else:
+                print(ob.dequeue(),end=" ")
+            i+=1
+
+        print()
+# } Driver Code Ends
