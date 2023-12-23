2e8dba7715b6568d7a928a8bb6eb075dc424f4f2
woongjoonchoi <oongjoon10@gmail.com>
2023-12-23 13:32:35 +0900

    feat : review
    Special stack
    
    four solutions
    
    improve space commplexity

---
+++ geeksforgeeks/Stack/review/23_12_23_min_stack_space_1.py
@@ -0,0 +1,71 @@
+#User function Template for python3
+
+class stack:
+    def __init__(self):
+        self.s=[]
+        self.minEle=None
+
+    def push(self,x):
+        
+        if not self.s :
+            self.minEle = x
+            
+        if self.minEle > x:
+            self.s.append(2*x-self.minEle)
+            self.minEle = x
+        else :
+            self.s.append(x)
+        
+                
+        #CODE HERE
+    
+    def pop(self):
+        if not self.s :return -1
+        
+        ans = self.s.pop()
+        
+        if ans < self.minEle :
+            temp = 2*self.minEle - ans
+            ans = self.minEle
+            self.minEle = temp
+        return ans
+        
+        #CODE HERE
+        
+
+    def getMin(self):
+        if not self.s : return -1
+        return self.minEle
+        #CODE HERE
+
+
+#{ 
+ # Driver Code Starts
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        q = int(input())
+
+        arr = [int(x) for x in input().split()]
+
+        stk=stack()  
+
+        qi = 0
+        qn=1
+        while qn <= q:
+            qt = arr[qi]
+
+            if qt == 1:
+                stk.push(arr[qi+1])
+                qi+=2
+            elif qt==2:
+                print(stk.pop(),end=' ')
+                qi+=1
+            else:
+                print(stk.getMin(),end=' ')
+                qi+=1
+            qn+=1
+        print()
+
+# } Driver Code Ends
---
+++ geeksforgeeks/Stack/review/23_12_23_min_stack_space_n_optimized.py
@@ -0,0 +1,64 @@
+#User function Template for python3
+
+#Function to push all the elements into the stack.
+min_stack = []
+def _push(a,n):
+    global min_stack
+    min_val = 10000000
+    st = []
+    
+    for ele in a :
+        st.append(ele)
+        
+        if min_val > ele : 
+            min_val = min(min_val,ele)
+            min_stack.append(min_val)
+            
+    return st
+        
+    # code here
+
+
+#Function to print minimum value in stack each time while popping.    
+def _getMinAtPop(stack):
+    global min_stack
+    ans = ""
+    while stack :
+        val = stack.pop() 
+        ans += str(min_stack[-1])+ " "
+        if val == min_stack[-1] : 
+            min_stack.pop()
+    print(ans,end="")
+    # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        stack =  _push(a,n) # our stack to be used
+        _getMinAtPop(stack) # print elements of stack as required
+        print()
+
+# } Driver Code Ends
